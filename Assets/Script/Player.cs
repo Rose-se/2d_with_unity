36831601e65779e61d910a914745fb4cbfab9ea8
Rose <nanping0948417786@gmail.com>
2023-12-07 11:09:02 +0700

    [TASK][GAMEOBJECT_MOVEMENT[SCRIPT]

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    //public float offset;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {   
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f,rb.velocity.y);
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x ,16f);
+        }
+    }
+    public class anim_update
+    {
+
+    }
+
+    public class attack
+    {
+    }
+
+}
