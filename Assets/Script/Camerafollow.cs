283960d7af7d341915457762a8c8aed0996d7c55
Rose <nanping0948417786@gmail.com>
2024-01-03 17:32:20 +0700

    [TASK][ANIMATION][ANIMATION]

--- Assets/Script/Camerafollow.cs
+++ Assets/Script/Camerafollow.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
-{   
-    [SerializeField] private float xSpeed = 2f;
-    [SerializeField] private float ySpeed = 1.5f; // Adjust this value to control the Y-axis movement speed
+{
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Transform target;
 
     void Update()
     {
-        float targetX = Mathf.Lerp(transform.position.x, target.position.x, xSpeed * Time.deltaTime);
-        float targetY = Mathf.Lerp(transform.position.y, target.position.y, ySpeed * Time.deltaTime);
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
 
-        Vector3 targetPosition = new Vector3(targetX, targetY, -10f);
+        // Check if the target is moving left or right
+        if (Mathf.Abs(target.position.x - transform.position.x) > 2.5f)
+        {
+            targetX = Mathf.Lerp(transform.position.x, target.position.x, moveSpeed * Time.deltaTime);
+        }
+
+        // Check if the target is moving up or down
+        if (Mathf.Abs(target.position.y - transform.position.y) > 10f)
+        {
+            targetY = Mathf.Lerp(transform.position.y, target.position.y, moveSpeed * Time.deltaTime);
+        }
+
+        Vector3 targetPosition = new Vector3(targetX, targetY, -15f);
 
         transform.position = targetPosition;
     }
