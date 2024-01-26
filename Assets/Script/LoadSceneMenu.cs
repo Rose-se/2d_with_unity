15cde0f205b61c6b048ce117826a1f6dd4f33644
Rose <76522002+Rose-se@users.noreply.github.com>
2024-01-26 15:05:57 +0700

    [TASK][UI][FIX_EVERYTING]

--- Assets/Script/LoadSceneMenu.cs
+++ Assets/Script/LoadSceneMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadSceneMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
